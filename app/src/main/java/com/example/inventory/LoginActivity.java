aa31f35e1e4a2a282deb85e55e38b6768f112d45
aaryak537 <aaryahkadam@gmail.com>
2026-07-07 16:44:04 +0530

    login page backend

--- app/src/main/java/com/example/inventory/LoginActivity.java
+++ app/src/main/java/com/example/inventory/LoginActivity.java
@@ -1,6 +1,14 @@
 package com.example.inventory;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,11 +17,61 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 public class LoginActivity extends AppCompatActivity {
 
+    EditText email,password;
+    Button login;
+    ImageView back;
+    TextView forget,signup;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
+
+        back=findViewById(R.id.back);
+        email=findViewById(R.id.etEmail);
+        password=findViewById(R.id.etPassword);
+        login=findViewById(R.id.btnLogin);
+        forget=findViewById(R.id.tvForget);
+        signup=findViewById(R.id.tvSignup);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(LoginActivity.this,SignupActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(LoginActivity.this,SignupActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String mail = email.getText().toString();
+                String pass = password.getText().toString();
+
+                if (mail.equals("") && pass.equals("")) {
+                    Toast.makeText(LoginActivity.this, "Login Successful",
+                            Toast.LENGTH_SHORT).show();
+
+                    Intent intent = new Intent(LoginActivity.this,
+                            DashActivity.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(LoginActivity.this, "Invalid email or password.",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
 
     }
 }
